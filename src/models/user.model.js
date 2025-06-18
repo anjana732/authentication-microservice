8f8907b097d729f0e3511627669e21af74f5c4f6
anjana732 <anjanasingh1257@gmail.com>
2025-06-18 23:52:27 +0530

    Add User model

---
+++ src/models/user.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserSchema = mongoose.Schema(
+    {
+        username : {
+            type: String,
+            required : [true, 'Please Enter usename'],
+            unique : true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required : [true, 'Please Enter an email'],
+            unique: true,
+            trim : true
+        },
+        password: {
+            type: String,
+            required : [true, 'Please Enter a password'],
+            unique: true,
+            trim : true
+        },
+        refreshToken: { 
+            type: String,
+        },
+    },{timestamp: true}
+);
+
+UserSchema.pre('save', async function (next){
+    if(!this.isModified('password')) next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+UserSchema.methods.mathchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import app from "./app.js";
 import logger from "./utils/logger.js";
+import connectDB from "./config/db.js";
 
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, ()=>{
     logger.info(`Server is running on port : ${PORT}`)
