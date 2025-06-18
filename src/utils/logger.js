21536eeaf33908d72f1059d0cf153dd8d52c1ff0
anjana732 <anjanasingh1257@gmail.com>
2025-06-18 14:59:44 +0530

    Setup Logger file

---
+++ src/utils/logger.js
@@ -0,0 +1,28 @@
+import winston from "winston";
+
+const logFormat = printf(({level, message, timestamp, stack}) => {
+    return `${timestamp} ${level} :  ${stack || message}`
+})
+
+const logger = winston.createLogger({
+    level: process.env.NODE_ENV === 'development' ? 'debug': 'info',
+    format: combine(
+        colorize({ all: true }), 
+        timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+        align(),
+        logFormat
+    ),
+    transports : [ 
+        new winston.transports.Console(),
+        new winston.transports.File({filename: 'logs/error.log', level: 'error'}),
+        new winston.transports.File({filename: 'logs/combined.log'})
+    ],
+    exceptionHandlers: [
+        new winston.transports.File({ filename: 'logs/exceptions.log' })
+    ],
+    rejectionHandlers: [
+        new winston.transports.File({ filename: 'logs/rejections.log' })
+    ]
+})
+
+export default logger;
