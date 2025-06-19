93b8061ffc16cdbe31cac1e2daa822bed294e520
anjana732 <anjanasingh1257@gmail.com>
2025-06-19 19:40:15 +0530

    Add auth controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,25 @@
+import signUpService from "../services/signup.service.js";
+import loginService from "../services/login.service.js";
+
+const signUp = async (req, res) => {
+    try {
+        const response = await signUpService(req.body); 
+        return res.status(response.status).json(response.body);
+    } catch (err) {
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+const login = async (req, res) => {
+    try {
+        const response = await loginService(req.body);
+        return res.status(response.status).json(response.body);
+    } catch (err) {
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+export default {
+    signUp,
+    login
+};
