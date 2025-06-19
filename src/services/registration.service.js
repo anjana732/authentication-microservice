683259304d5bfc3adaae54192a4b9a6a2943882b
anjana732 <anjanasingh1257@gmail.com>
2025-06-19 16:53:01 +0530

    Add signup logic

--- src/services/registration.service.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
-import logger from "../utils/logger";
-import jwt from "jsonwebtoken";
-
---
+++ src/services/signup.service.js
@@ -0,0 +1,24 @@
+import logger from "../utils/logger";
+import User from "../models/user.model";
+
+const signUp = async (req, res) => {
+    try{
+        const {username, email, password} = req.body;
+        const existing = await User.findOne({email});
+
+        if(!existing){
+            logger.info(`User already Exist`);
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        const newUser = await User.create({username, email, password})
+        logger.info(`New User Created`, newUser);
+        return res.status(200).json({message: 'User created successfully', User: newUser})
+
+    }catch(err){
+        logger.error(`Signup error: `, err);
+        return res.status(500).json({error: `error creating user`})
+    }
+}
+
+export default signUp;
